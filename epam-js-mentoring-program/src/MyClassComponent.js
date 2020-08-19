4d90a89d72dfe49fe01b6517b63990901b19ef78
Mateusz Szymanski <Mateusz_Szymanski@epam.com>
2020-08-19 16:03:02 +0200

    PureComponent and Class Component

---
+++ epam-js-mentoring-program/src/MyClassComponent.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class MyClassComponent extends Component {
+    render() { 
+        return <div>Hello from MyClassComponent</div>;
+    }
+}
---
+++ epam-js-mentoring-program/src/MyReactPureComponent.js
@@ -0,0 +1,6 @@
+import React, { PureComponent } from 'react';
+export default class MyReactPureComponent extends PureComponent {
+    render() { 
+        return <span>Hello from PureComponent</span>;
+    }
+}
